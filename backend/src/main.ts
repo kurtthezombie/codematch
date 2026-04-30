3667e636d411da6634dbd5d9ef2c885fe831039f
kurtthezombie <kurt.desmond619@gmail.com>
2026-04-30 22:09:47 +0800

    chore(docker): add development compose setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,8 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist:true }));
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
